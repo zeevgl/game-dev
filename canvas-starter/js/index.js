e867c89648165b05782bee74a83411d6621a5adb
zeevg <zeevg@wix.com>
2022-04-21 11:00:15 +0300

    starter POC

---
+++ canvas-starter/js/index.js
@@ -0,0 +1,25 @@
+let canvas = document.getElementById("gameScreen");
+const ctx = canvas.getContext("2d");
+ctx.imageSmoothingEnabled = false;
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
+const game = new Game(GAME_WIDTH, GAME_HEIGHT);
+
+let lastTime = 0;
+function gameLoop(timestamp) {
+
+    let deltaTime = timestamp - lastTime;
+    lastTime = timestamp;
+    //console.log('timestamp = ', timestamp, deltaTime);
+
+    ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+
+    game.update(deltaTime, timestamp);
+    game.draw(ctx);
+
+    requestAnimationFrame(gameLoop);
+}
+
+requestAnimationFrame(gameLoop);
