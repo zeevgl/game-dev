ec286db607c5fac3ef7cefbdf9753261cff59df0
Zeev G <maijor@gmail.com>
2022-04-25 00:55:35 +0300

    camera edge

--- movement/js/game.js
+++ movement/js/game.js
@@ -20,8 +20,8 @@ class Game {
       //follow
       context.translate(-this.player.x - PLAYER_SIZE + canvas.width / 2, 0);
     } else if ( this.player.x + PLAYER_SIZE >= 900) {
-      //getting to right eadge. WIP
-      context.translate(-this.player.x - PLAYER_SIZE + this.player.x * 0.1 + canvas.width / 2, 0);
+      //getting to right edge. WIP
+      context.translate(-this.player.x - PLAYER_SIZE + this.player.x * 0.2 + canvas.width / 2, 0);
     }
 
     //  else {
@@ -33,7 +33,7 @@ class Game {
   }
 
   drawRect(context) {
-    for (let i = 0; i < 30; i++) {
+    for (let i = 0; i < 300; i++) {
       context.fillStyle = i % 2 === 0 ? '#ff0000' : '#00ff00';
       context.fillRect(50 * i + i * 50, 20, 10, 800);
     }
